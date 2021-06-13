26e92ae02afd4cbef83912b9cd13b105b9b243b8
RajKKapadia <raajforyou@gmail>
2021-06-13 11:33:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello friends,
+
+Welcome to this repository, to see how to use this repository please see this [video](https://youtu.be/fApN4Cd5ZEU).
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const sendMail = async (message, receiverEmail, subject) => {
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        service: 'gmail',
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: receiverEmail,
+        subject: subject,
+        text: message
+    });
+
+    console.log("Message sent: %s", info.messageId);
+};
+
+sendMail('Hello from the nodemaialer.', 'rajkapadiagecmodasa@gmail.com', 'Test subject');
